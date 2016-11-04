8a33a5e851ba93bd67edb778ea33b70961548f42
Phaninder Pasupula <pasupulaphani@gmail.com>
2016-11-04 00:06:24 +0000

    new test

--- test/index.js
+++ test/index.js
@@ -5,12 +5,36 @@ describe("redisStore", function () {
 
   var redisStore = new RedisStore("testStore");
 
-  it("set", function (done) {
+  describe("set", function () {
+    it("set", function (done) {
 
-    redisStore.set("key", "value")
-      .then(function (test) {
-        test.should.be.ok();
-        done();
-      });
+      redisStore.set("key", "value")
+        .then(function (test) {
+          test.should.be.ok();
+          done();
+        });
+    });
+  });
+
+  describe("get", function () {
+
+    var key = "chuck-norris";
+    var value = "alias: superman";
+
+    before(function (done) {
+      redisStore.set(key, value)
+        .then(function () {
+          done();
+        });
+    });
+
+    it("get", function (done) {
+
+      redisStore.get(key)
+        .then(function (v) {
+          v.should.be.equal(value);
+          done();
+        });
+    });
   });
 });
